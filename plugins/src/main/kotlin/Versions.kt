d4878d5c023e179a6384e3b16622608088744b5f
shahroz-wilder <shrozali@outlook.com>
2025-07-25 14:50:31 +0500

    fix: rewards settings screen title

--- plugins/src/main/kotlin/Versions.kt
+++ plugins/src/main/kotlin/Versions.kt
@@ -47,10 +47,10 @@ private const val versionMinor = 10
 // Note: even values are reserved for regular release, odd values for hotfix release.
 // When creating a hotfix, you should decrease the value, since the current value
 // is the value for the next regular release.
-private const val versionPatch = 0
+private const val versionPatch = 1
 
 object Versions {
-    const val VERSION_CODE = 75
+    const val VERSION_CODE = 76
     const val VERSION_NAME = "$versionMajor.$versionMinor.$versionPatch"
     const val COMPILE_SDK = 35
     const val TARGET_SDK = 35
